aee1561ea0001db7fb43f0a0770ce6f3d6aaf331
zzw <33030726+zzwgo@users.noreply.github.com>
2019-06-23 23:26:34 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+var user = new Object();
+user.name="John"
+user.surname="Mike"
+user.name="Peter"
+delete  user.name
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+console.log(fruit.apple+fruit.pear+fruit.peach);
